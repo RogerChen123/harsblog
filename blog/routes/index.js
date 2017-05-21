a471d71d6878029c9078825b6de2fd094fa4344e
cyl <chenyilong_1030@126.com>
2017-05-21 14:09:06 +0800

    新增注册，登录，发表页面

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,10 +1,29 @@
 /* GET home page. */
-module.exports = function(app){
+module.exports = function (app) {
 
-  app.get("/",function(req,res){
+    app.get("/", function (req, res) {
 
-      res.render("index",{
-        title:"chenyilong"
-      })
-  })
+        res.render("index", {
+            title: "首页",
+            blogName: "harsbog个人博客系统"
+        })
+    });
+    app.get("/reg", function (req, res) {
+
+        res.render("reg", {
+            title: "注册"
+        })
+    });
+    app.get("/login", function (req, res) {
+
+        res.render("login", {
+            title: "登录"
+        })
+    });
+    app.get("/post", function (req, res) {
+
+        res.render("post", {
+            title: "发表文章"
+        })
+    });
 };
